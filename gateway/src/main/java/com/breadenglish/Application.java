605466efde4cc6b98062fc41ece8c48e08f7ad8c
zhaohuaan <zhaohuaan0925@163.com>
2016-02-23 11:44:04 +0800

    去掉无用注解

--- gateway/src/main/java/com/breadenglish/Application.java
+++ gateway/src/main/java/com/breadenglish/Application.java
@@ -7,13 +7,11 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
 @ComponentScan
 @EnableAutoConfiguration
 @EnableScheduling
-@EnableWebMvc
 @EnableJSONDoc
 public class Application implements CommandLineRunner {
 
